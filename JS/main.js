c15e86a9bc71ff9268bbc89b61570b54021a4e27
Ayers <gayers@btutilities.com>
2017-08-15 08:16:35 -0500

    Section 3, Lecture 44

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,49 @@
-console.log(document.querySelector("#different"));
+/*var parent = document.querySelector('.parent');
+var div = document.createElement('div');
+
+div.textContent = "My new content";
+div.style.color = "red";
+div.style.fontSize = "20px";
+console.log(div);
+
+parent.appendChild(div);*/
+
+
+/*var button = document.querySelector('.myButton');
+
+function click1(){
+	console.log("someone clicked me");
+};
+
+function click2(){
+	console.log("doing something else");
+	button.removeEventListener("click", click1);
+};
+
+button.addEventListener("click", click1);
+button.addEventListener("click", click2);*/
+
+/*document.addEventListener("click", showPopUp);
+
+function showPopUp(){
+	alert("Buy me");
+}*/
+
+var child = document.querySelector(".child");
+var parent = document.querySelector(".parent");
+
+child.addEventListener("click", showChild);
+parent.addEventListener("click", showParent);
+
+function showChild(event) {
+	event.stopPropagation();
+
+	event.target.style.background = 'blue';
+	
+	console.log("I am green");
+}
+
+
+function showParent() {
+	console.log("I am red");
+}
